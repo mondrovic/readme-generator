59f08d0f863f7167cc0dfe66867ac07cbf40241d
Matthew O <mo@pop-os.localdomain>
2020-09-26 21:31:58 -0700

    readme is generated with basic information

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,90 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateToC = (toc) => {
+  if (!toc) {
+    return "";
+  }
+  return `
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#Credits)
+  * [License](#License)
+  * [Questions](#questions)
+  `;
+};
 
+const generateLicense = (licenseArr) => {
+  return `
+  ${licenseArr
+    .map(({ license }) => {
+      return `
+    License used ${license}
+    `;
+    })
+    .join("")}
+  `;
+};
+
+const generateContact = (contactArr) => {
+  return `
+  ${contactArr
+    .map(({ fullName, email, github }) => {
+      return `
+    Any questions or inquiries can go to ${email}
+
+    ${fullName}
+    https://githbub.com/${github}
+    `;
+    })
+    .join("")}
+  `;
+};
+
+const generateContr = (contrArr) => {
+  return `
+  ${contrArr
+    .map(({ credits }) => {
+      return `
+    ${credits}
+    `;
+    })
+    .join("")}
+  `;
+};
+
+module.exports = (templateData) => {
+  const { contact, contrib, license, ...rmBody } = templateData;
+
+  return `
+  # ${rmBody.title}
+
+  ## Description
+
+  ${rmBody.description}
+
+  ${generateToC(rmBody.confirmToC)}
+
+  ## Installation
+
+  ${rmBody.installation}
+
+
+  ## Usage
+
+  ${rmBody.usage}
+
+
+  ## Credits
+
+  
+  ${generateContr(contrib)}
+
+
+  ## License
+
+  ${generateLicense(license)}
+
+
+  ## Inquiries
+
+  ${generateContact(contact)}
 `;
-}
-
-module.exports = generateMarkdown;
+};
